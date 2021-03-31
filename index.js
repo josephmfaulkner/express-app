071913de9579c0a9d4d9e42d1bbd46c083ac832f
josephmfaulkner <josephmfaulkner@gmail.com>
2021-03-30 19:54:31 -0700

    status

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
+const app = express(); 
 
-app.listen(process.env.PORT || 3000);
+app.get('/', (request, response) => {
+  response.json({
+    message: 'Hello World!'
+  })
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));

7dc9200c445ac60ae21106be38cadc933fb7e96f
josephmfaulkner <josephmfaulkner@gmail.com>
2021-03-31 19:09:27 -0700

    Removed Extra Files

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express(); 
@@ -12,11 +11,3 @@ app.get('/', (request, response) => {
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-/*
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
